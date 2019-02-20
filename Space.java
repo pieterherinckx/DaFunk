2dc7a8a4842f937ecb6e8159647b7f9bc7b406e9
pieterherinckx <pieterherinckx@atheneumtienen.be>
2019-02-20 16:19:56 +0100

    Create Space.java

---
+++ Space.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+/**
+ * Write a description of class Space here.
+ * 
+ * @author Pieter Herinckx 
+ * @version 1.0
+ */
+public class Space  extends World
+{
+
+    /**
+     * Constructor for objects of class Space.
+     * 
+     */
+    public Space()
+    {    
+        super(1201, 801, 1);
+    }
+
+}
